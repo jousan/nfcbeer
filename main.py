4dbca6445c8d4702f8c57317a1b04637bfed3131
Carles Mateu <carlesm@carlesm.com>
2016-05-13 17:29:48 +0200

    Random users and beer

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+import random
+
+users = [
+    "0001",
+    "0002",
+    "0003",
+    "0004",
+    "0005",
+    "0006"
+]
 
 class FlowControl(object):
     """Controlling FlowControl"""
@@ -8,35 +18,65 @@ class FlowControl(object):
         self.previousTime = 0
         self.service = 0
         self.total = 0
+        self.user = -2
+
+    def _get_user(self):
+        us = random.random()
+        if us < 0.8:
+            user = users[random.randint(0,5)]
+            return user
+        else:
+            return "Anonymous"
+
 
     def update(self, channel):
         tim = time.time()
         delta = tim - self.previousTime
-        if delta < 500:
+        if delta < 0.50:
+            print ".",
             self.hertz = 1000.0 / delta
             self.flow = self.hertz / 450.0  # Liter/Second
             service = self.flow * (delta / 1000.0)
             self.service  += service
             self.total    += service
         else:
+            if self.user != -2:
+                print "+"
+                print "user", self.user, " drank ", self.service
             # Guardar a usuari self.service
             self.service = 0
             self.user = self._get_user()
 
         self.previousTime = tim
 
+    def _debug_dump(self):
+        print "DBG: TOTAL: ", self.total, "Servei:", self.service
 
 if __name__ == "__main__":
     fl = FlowControl()
 
-	GPIO.setmode(GPIO.BCM) # use real GPIO numbering
-	GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(22, GPIO.RISING, callback=fl.update, bouncetime=20)
+    # GPIO.setmode(GPIO.BCM) # use real GPIO numbering
+    # GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.add_event_detect(22, GPIO.RISING, callback=fl.update, bouncetime=20)
 
+    random.seed(1)
+    c = 0
     try:
         while True:
-            time.sleep(1)
+            a = random.random()
+            if a < 0.3:
+                fl.update(1)
+
+            if a < 0.009:
+                time.sleep(0.550)
+
+            time.sleep(0.002)
+            c += 1
+            if c > 1000:
+                c = 0
+                fl._debug_dump()
     except:
         pass
     finally:
-        GPIO.cleanup()
+        # GPIO.cleanup()
+        pass
