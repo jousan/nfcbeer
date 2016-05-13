61d987e99e72c373cc15f79e9312ed537162365c
Carles Mateu <carlesm@carlesm.com>
2016-05-13 18:25:08 +0200

    Added

--- main.py
+++ main.py
@@ -11,23 +11,57 @@ users = [
     "0006"
 ]
 
+
+class FakeNFCReader(object):
+    """
+    Fake NFC Reader class
+
+    Mimicks an NFC Reader
+    """
+
+    uids = [
+    "0000000000",
+    "1111111111",
+    "2222222222",
+    "3333333333",
+    "4444444444",
+    "5555555555",
+    "6666666666",
+    "7777777777",
+    "8888888888",
+    "9999999999"
+    ]
+
+    def __init__(self):
+        self.uid = None
+
+    def is_card_present(self):
+        r = random.random()
+        if r < 0.8:
+            self.uid = self.uids[random.randint(0,9)]
+        else:
+            self.uid = None
+        return self.uid
+
+    def read_uid(self):
+        return self.uid
+
+
 class FlowControl(object):
     """Controlling FlowControl"""
-    def __init__(self):
+    def __init__(self, nfc=None):
         super(FlowControl, self).__init__()
         self.previousTime = 0
         self.service = 0
         self.total = 0
         self.user = -2
+        self.nfc = nfc
 
     def _get_user(self):
-        us = random.random()
-        if us < 0.8:
-            user = users[random.randint(0,5)]
-            return user
-        else:
-            return "Anonymous"
-
+        if self.nfc is not None:
+            if self.nfc.is_card_present():
+                return self.nfc.read_uid()
+        return "None"
 
     def update(self, channel):
         tim = time.time()
@@ -53,11 +87,14 @@ class FlowControl(object):
         print "DBG: TOTAL: ", self.total, "Servei:", self.service
 
 if __name__ == "__main__":
-    fl = FlowControl()
+
+    nf = FakeNFCReader()
+    fl = FlowControl(nfc=nf)
 
     # GPIO.setmode(GPIO.BCM) # use real GPIO numbering
     # GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
     # GPIO.add_event_detect(22, GPIO.RISING, callback=fl.update, bouncetime=20)
+
 
     random.seed(1)
     c = 0
@@ -75,8 +112,8 @@ if __name__ == "__main__":
             if c > 1000:
                 c = 0
                 fl._debug_dump()
-    except:
-        pass
+    except Exception as e:
+        print e
     finally:
         # GPIO.cleanup()
         pass
